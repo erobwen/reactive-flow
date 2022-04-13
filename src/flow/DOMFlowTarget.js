40f9c9719280f1979cb9a3f1e3c79e6336e88932
rore03 <robert.renbris@handelsbanken.se>
2022-04-13 09:14:23 +0200

    New file.

--- src/flow/DOMFlowTarget.js
+++ src/flow/DOMFlowTarget.js
@@ -1,5 +1,6 @@
 import { observable, world, repeat, readFlowArguments, Flow } from "./Flow";
-import { Text, Row, Button, PrimitiveFlow } from "./PrimitiveFlow";
+import { DOMButton } from "./DOMFlowPrimitive";
+
 const log = console.log;
 
 function clearNode(node) {
@@ -131,27 +132,3 @@ export class DOMFlowTarget extends FlowTarget {
   }
 }
 
-
-/**
- * DOM primitive flows
- */
-export class DOMButton extends PrimitiveFlow {
-  setProperties({onClick, text}) {
-    // log("button set properties");
-    this.onClick = () => {
-      console.log("clicked at: " + JSON.stringify(this.getPath()))
-      onClick();
-    }
-    this.text = text; 
-  }
-
-  createEmptyDomNode() {
-    return document.createElement("button");
-  }
-
-  buildDomNode(element) {
-    element.onclick = this.onClick;
-    if (element.childNodes.length === 0) element.appendChild(document.createTextNode(''));
-    element.lastChild.nodeValue = this.text;
-  }
-}
