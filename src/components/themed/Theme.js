4eb399ceefc046f87d3dd28dcd936c1ea55b8c00
HAUD\robert.renbris <robert.renbris@haud.com>
2023-08-16 13:13:55 +0200

    ...

--- src/components/themed/Theme.js
+++ src/components/themed/Theme.js
@@ -1,10 +1,12 @@
 import { div } from "../../flow.DOMTarget/BasicHtml";
+import { model } from "../../flow/Flow";
 import { textInputField as basicTextInputField } from "../basic/BasicWidgets";
 import { button as basicButton } from "../basic/BasicWidgets";
 import { modernButton } from "../modern/ModernButton";
 
 
 export function button(...parameters) {
+  // return theme.button.apply(null, parameters);
   // return basicButton.apply(null, parameters); 
   return modernButton.apply(null, parameters);
 }
@@ -14,3 +16,17 @@ export function textInputField(...parameters) {
   return basicTextInputField.apply(null, parameters);
   // return modernTextField.apply(null, parameters);
 }
+
+export const theme = model({
+  // Alternatives
+  buttonAlternatives: {
+    modernButton,
+    basicButton
+  },
+  textInputFieldAlternatives: {
+    basicTextInputField
+  },
+
+  button: modernButton,
+  textInputField: basicTextInputField 
+});
